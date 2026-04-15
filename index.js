f921ec18aba0057103e5438d5a1d657f74095c19
RomanAli2 <romanali87866677@gmail.com>
2026-04-15 18:03:46 -0400

    clean first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const express= require("express")
+const app=express()
+const data=require('./MOCK_DATA.json')
+const fs= require("fs")
+const PORT=5000
+app.use(express.urlencoded({extended:false}))
+app.use(express.json())
+app.get('/',(req,res)=>{
+    res.json(data)
+})
+app.post('/Post',(req,res)=>{
+    const newUser=req.body
+    data.push({...newUser,id: data.length+1})
+
+    fs.writeFile('./MOCK_DATA.json',JSON.stringify(data),(err)=>{
+        if(err){
+           return res.status(500).json({message: "Error saving data"})
+        }
+  res.json({ message: "Data added successfully", user: newUser })
+    })
+})
+app.get('/:id',(req,res)=>{
+    const id=Number(req.params.id)
+    const user=data.find((user)=>user.id==id)
+    return res.json(user)
+})
+
+app.listen(PORT,()=>console.log("HI server"))
