6198125c5ae5304bcbd228dda1eb4fb4e44b1df1
RomanAli2 <romanali87866677@gmail.com>
2026-04-17 08:56:30 -0400

    'first'

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express= require("express")
+import cors from "cors";
 const app=express()
 const data=require('./MOCK_DATA.json')
 const fs= require("fs")
 const PORT=5000
+app.use(cors());
+app.use(express.json());
 app.use(express.urlencoded({extended:false}))
 app.use(express.json())
 app.get('/',(req,res)=>{
